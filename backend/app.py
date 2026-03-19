621d9ba389964d95aa0c7cbeaeed1bb99f32caf8
Christian Offenheiser <118592069+Coff1199@users.noreply.github.com>
2026-03-19 14:19:49 -0400

    Initial commit and project environment setup for front and backend

---
+++ backend/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+app = Flask(__name__)
+CORS(app)
+@app.route("/api/message")
+def home():
+    return jsonify({"message":"running..."})
+
+load_dotenv()
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+conn = engine.connect()
+print("Connected to DB")
+
+if __name__ == "__main__":
+    app.run(debug=True)
