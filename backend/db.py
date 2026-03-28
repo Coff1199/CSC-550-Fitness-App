d4d78be16f1d85e8fa00ef39a77ed328242c87b0
Christian Offenheiser <118592069+Coff1199@users.noreply.github.com>
2026-03-28 12:47:12 -0400

    added view goals to backend end api and created test files for it

---
+++ backend/db.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from flask import jsonify, Blueprint
+
+# make a blueprint of db
+db_bp = Blueprint("database", __name__)
+
+@db_bp.route('/api/message', methods=["GET"])
+def home():
+    """
+    Function to show if flask is connected
+    :return: jsonify message for running
+    """
+    return jsonify({"message":"running..."})
+
+# get the databse
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# create the engine and connect to database 
+engine = create_engine(os.getenv("DATABASE_URL"))
+conn = engine.connect()
+print("Connected to DB")
