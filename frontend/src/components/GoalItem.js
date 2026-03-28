f650bf43814ec4a292fdd5fde4c4c6f4d02f0ded
Christian Offenheiser <118592069+Coff1199@users.noreply.github.com>
2026-03-28 13:36:53 -0400

    connected frontend to backend api call

---
+++ frontend/src/components/GoalItem.js
@@ -0,0 +1,24 @@
+
+export default function GoalItem(properties) {
+    /**
+     * React component that creates a goal item 
+     * params: key, id : integers, goalName, goalDesc : strings, react components for 
+     * returns a div goal item with  the goals name, description, and edit and delete buttons
+     **/
+
+    return(
+        <>
+            <div className="goal-item">
+            <h2>{properties.goalName}</h2>
+            <p>{properties.goalDesc}r</p>
+                <div className="goal-actions">
+                    {
+                        // TODO: add any aadditional componenets for edit and delete
+                    }
+                    <button className="edit-btn">Edit temp</button>
+                    <button className="delete-btn">Delete temp</button>
+                </div>
+            </div>
+        </>
+    );
+}
