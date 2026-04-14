31045d048931045c6cea2cbda083872da08e7dd8
Christian Offenheiser <118592069+Coff1199@users.noreply.github.com>
2026-04-13 23:01:47 -0400

    small fixes

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,8 @@ from routes.auth import auth_bp
 from routes.add_goal import add_goal_bp
 from routes.delete_goal import delete_goal_bp
 from routes.edit_goal import edit_goal_bp
+from routes.dashboard import dashboard_bp
+from routes.log_workout import log_workout_bp
 from db import db_bp
 from datetime import timedelta
 
@@ -28,10 +30,13 @@ CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}}, supports_
 
 # register your blueprints
 app.register_blueprint(goals_bp)
-app.register_blueprint(auth_bp)  # NEW: Register auth blueprint
+app.register_blueprint(auth_bp)  
 app.register_blueprint(add_goal_bp)
-app.register_blueprint(dashboard_bp)
 app.register_blueprint(db_bp)
+app.register_blueprint(delete_goal_bp)
+app.register_blueprint(edit_goal_bp)
+app.register_blueprint(dashboard_bp)
+app.register_blueprint(log_workout_bp)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
